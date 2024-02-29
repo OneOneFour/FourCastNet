7df98f04327f550da52f740a5bc4803e1790dd6b
Robert King <robcking@stanford.edu>
2024-02-28 20:13:51 -0800

    please please PLEASE god no

--- data_process/generate_h5.py
+++ data_process/generate_h5.py
@@ -39,7 +39,7 @@ def add_feature(src,dest,channel_idx,variable_name,src_idx=0,frmt='nc'):
     if frmt == 'nc':
         fsrc = DS(src,'r',format="NETCDF4").variables[variable_name]
     elif frmt == 'h5':
-        fsrc = h5py.File(src,'r')[variable_name]]
+        fsrc = h5py.File(src,'r')[variable_name]
     with h5py.File(dest,'a') as fdest:
         if len(fsrc.shape) == 4:
             ims = fsrc[:,src_idx]
